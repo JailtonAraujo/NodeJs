bd1798e981fb7b841728e1ee90acad5241bf3c77
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2023-02-07 16:10:53 -0300

    update

--- 9_integrando_sequelize/index.js
+++ 9_integrando_sequelize/index.js
@@ -4,6 +4,7 @@ const engine = require('express-handlebars')
 const conn = require('./db/conf');
 
 const User = require('./models/User');
+const Address = require('./models/Address');
 
 const app = express();
 
@@ -24,6 +25,26 @@ app.use(express.static('public'));
 app.get("/users/create", (req, res)=>{
 
     res.render("adduser");
+})
+
+app.post("/addess/insert", async (req,res)=>{
+
+    const UserId = req.body.UserId;
+    const street = req.body.street;
+    const city = req.body.city;
+    const number = req.body.number;
+
+    const addressData = {
+        UserId,
+        street,
+        city,
+        number
+    }
+
+    await Address.create(addressData);
+
+    res.redirect(`/user/update/${UserId}`);
+
 })
 
 app.post("/users/create", async (req, res)=>{
@@ -47,8 +68,79 @@ app.get("/", async (req, res)=>{
     res.render('home',{users});
 })
 
+app.post("/users/update", async(req, res)=>{
 
-conn.sync().then(()=>{
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    const id = req.body.id;
+    let newsletter = req.body.newsletter;
+
+    newsletter = newsletter ? true : false;
+
+    const userData ={
+        id,
+        name,
+        occupation,
+        newsletter
+    }
+
+    await User.update(userData,{where:{id}});
+
+    res.redirect("/");
+
+})
+
+app.post("/address/delete/:id", async (req, res)=>{
+
+    const id = req.params.id;
+
+    const UserId = req.body.UserId;
+
+    console.log(UserId)
+
+    await Address.destroy({where:{id}});
+
+    res.redirect("/user/update/"+UserId);
+
+})
+
+app.post("/users/delete/:id", async (req, res)=>{
+
+    const id = req.params.id;
+
+    await User.destroy({where:{id:id}});
+
+    res.redirect("/")
+
+})
+
+app.get("/user/update/:id", async (req, res)=>{
+
+    const id = req.params.id;
+
+
+    const user = await User.findOne({include:Address, where:{id}});
+
+    console.log(user.get({plain:true}));
+
+    res.render('useredit',{user : user.get({plain:true})});
+
+})
+
+app.get("/users/:id", async (req, res)=>{
+
+    const id = req.params.id;
+
+    const user = await User.findOne({raw:true ,where:{id:id}});
+
+    res.render("userview",{user});
+
+})
+
+conn
+// .sync({force:true})
+.sync()
+.then(()=>{
 
     app.listen(3000)
 
---
+++ 9_integrando_sequelize/models/Address.js
@@ -0,0 +1,26 @@
+const db = require("../db/conf");
+
+const { DataTypes } = require('sequelize');
+const User = require('./User');
+
+const Address = db.define('Address',{
+
+    street:{
+        type: DataTypes.STRING,
+        require:true
+    },
+    number:{
+        type: DataTypes.STRING,
+        require:true
+    },
+    city:{
+        type: DataTypes.STRING,
+        require:true
+    }
+
+})
+
+User.hasMany(Address);
+Address.belongsTo(User);
+
+module.exports = Address;
