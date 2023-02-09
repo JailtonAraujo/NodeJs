ccb56a76f6021f81ec100834d149e39e0d3695bd
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2023-02-09 13:41:27 -0300

    Finalizado project toughts

--- 11_TOUGTHS/controllers/ToughtController.js
+++ 11_TOUGTHS/controllers/ToughtController.js
@@ -1,9 +1,41 @@
 const Tought = require('../models/Tought');
 const User = require('../models/User');
 
+const { Op } = require('sequelize');
+
 module.exports = class ToughtControler {
 
-    static async showToughts (req, res){ res.render('toughts/home');}
+    static async showToughts (req, res){
+        
+        let search = '';
+
+        if(req.query.search){
+            search = req.query.search;
+
+        }
+
+        let order = 'DESC';
+
+        order = req.query.order === 'old' ? 'ASC' : 'DESC'; 
+        
+        const toughtsData = await Tought.findAll({
+            include:User, 
+            where:{title:{[Op.like]: `%${search}%`}},
+            order: [['createdAt',order]],
+        });
+        
+        const toughts = toughtsData.map((result)=>result.get({plain:true}));
+
+        let toughtsQty = toughts.length;
+
+        if(toughtsQty === 0){
+            toughtsQty = false;
+        }
+
+        res.render('toughts/home',{toughts, search, toughtsQty});
+    
+    
+    }
 
     static async dashboard(req, res){
         
