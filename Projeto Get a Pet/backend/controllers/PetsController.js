8b0208efbb911e0a0ffb26967dfd2bdd49ad955f
Jailton de Araújo Santos <jailtonraneimoney@gmail.com>
2023-02-14 23:26:49 -0300

    update

--- Projeto Get a Pet/backend/controllers/PetsController.js
+++ Projeto Get a Pet/backend/controllers/PetsController.js
@@ -1,4 +1,5 @@
 const Pet = require('../models/Pet');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports = class PetsController {
 
@@ -70,6 +71,167 @@ module.exports = class PetsController {
         } catch (error) {
             res.status(501).json({message:error});
         }
+
+    }
+
+    static async getAll(req,res){   
+
+        const pets = await Pet.find().sort('-createdAt');
+
+        res.status(200).json(pets);
+
+    }
+
+
+    static async getAllUserPets(req,res){
+
+        const user = req.user;
+
+        const pets = await Pet.find({'user._id':user._id}).sort('-createdAt');
+
+        res.status(200).json(pets);
+
+    }
+
+    static async getAllUserAdoptions(req,res){
+
+        const user = req.user;
+
+        const pets = await Pet.find({'adopter._id':user._id}).sort('-createdAt');
+
+        res.status(200).json(pets);
+
+    }
+
+    static async getPetById(req,res){
+
+        const id = req.params.id;
+
+        //Validate ObjectId
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({message:"Id is not valid!"});
+            return;
+        }
+
+        const pet = await Pet.findById(id);
+
+        if(!pet){
+            res.status(404).json({message:"Pet not found!"});
+            return;
+        }
+
+        res.status(200).json(pet);
+    }
+
+    //Delete a Pet
+    static async delete (req,res){
+
+        const petId = req.params.id;
+
+        const user = req.user;
+
+        //verify pet id
+        if(!ObjectId.isValid(petId)){
+            res.status(422).json({message:"Id is not valid!"});
+            return;
+        }
+
+
+        //verify id exists a pet
+        const pet = await Pet.findById(petId);
+        if(!pet){
+            res.status(404).json({message:"Pet not found!"});
+            return;
+        }
+
+        //verify id pet belong user
+        if(user._id.toString() !== pet.user._id.toString()){
+            res.status(404).json({message:"Impossible process action, try again later!"});
+            return;
+        }
+
+        await Pet.findByIdAndRemove(petId);
+
+        res.status(200).json({message:"Pet been removed!"});
+
+    }
+
+    static async updatePet (req, res){
+
+        const id = req.params.id;
+
+        const {
+            name,
+            age,
+            wight,
+            color,
+            available
+        }=req.body;
+
+        const user = req.user;
+
+        const images = req.files;
+
+        const updateDate = {};
+        
+        //verify id exists a pet
+        const pet = await Pet.findById(id);
+        if(!pet){
+            res.status(404).json({message:"Pet not found!"});
+            return;
+        }
+
+         //verify id pet belong user
+         if(user._id.toString() !== pet.user._id.toString()){
+            res.status(404).json({message:"Impossible process action, try again later!"});
+            return;
+        }
+
+
+        //Validations
+        if(!name){
+            res.status(422).json({message:"Name is required!"});
+            return;
+        }else{
+            updateDate.name = name;
+        }
+
+        if(!age){
+            res.status(422).json({message:"Age is required!"});
+            return;
+        }else{
+            updateDate.age = age;
+        }
+
+        if(!wight){
+            res.status(422).json({message:"Wight is required!"});
+            return;
+        }else{
+            updateDate.wight = wight;
+        }
+
+        if(!color){
+            res.status(422).json({message:"Color is required!"});
+            return;
+        }else{
+            updateDate.color = color;
+        }
+
+        if(images.length === 0){
+            res.status(422).json({message:"Image is required!"});
+            return;
+        }else{
+            updateDate.images = [];
+            images.map((image)=>{
+                updateDate.images.push(image.filename);
+            })
+        }
+
+
+        await Pet.findByIdAndUpdate(id,updateDate);
+
+        res.status(200).json({message:"Pet been update."})
+
 
     }
 }
--- Projeto Get a Pet/backend/routes/PetsRoutes.js
+++ Projeto Get a Pet/backend/routes/PetsRoutes.js
@@ -5,6 +5,12 @@ const authGuard = require('../middlewares/authGuard');
 const uploadImage = require('../helpers/uploads-image');
 
 router.post('/create',authGuard,uploadImage.array('images'),petsController.create);
+router.get('/',petsController.getAll);
+router.get('/mypets',authGuard,petsController.getAllUserPets);
+router.get('/myadoptions',authGuard,petsController.getAllUserAdoptions);
+router.get('/:id',petsController.getPetById);
+router.delete('/:id',authGuard,petsController.delete);
+router.patch('/:id',authGuard,uploadImage.array('images'),petsController.updatePet)
 
 module.exports = router;
 
